632842c8b7621c7a58d97042a975036904bc5e64
JavierKipen <javier1kipen@gmail.com>
2019-11-05 08:36:46 -0300

    Versión para presentar
    Se le añadió overlapping con hamming y la posibilidad de tener input de la música. Esta es la versión para presentar

--- TP1Voz.py
+++ TP1Voz.py
@@ -12,10 +12,11 @@ b[0] = 1
 zi = signal.lfilter_zi(a, b) #Estados iniciales
 
 buffer_count = 0# Cuenta de cuantas veces se llamo al callback
+BUFFER_SIZE = 2048# Tamaño del ventaneo
+freq = 110#frecuencia inicial
 
-BUFFER_SIZE = 512# Tamaño del ventaneo
-
-freq = 100#frecuencia inicial
+prevIn=np.array(np.zeros((int(BUFFER_SIZE/2)),))
+prevOut=np.array(np.zeros((int(BUFFER_SIZE/2)),))
 
 def wav2Float(dataIn): #Del formato del wav (buffer de bytes en codif int16) a array de floats
     dataOut = np.frombuffer(dataIn, dtype=np.int16)
@@ -31,29 +32,34 @@ def float2wav(dataIn):
 # Callback se llama cuando hay "frames_per_buffer" nuevos samples en el wav abierto, y toma tambien esa cantiad de
 # muestras de audio del microfono.
 def callback(in_data, frame_count, time_info, flag):
-    global zi, buffer_count, BUFFER_SIZE, freq
+    global zi, buffer_count, BUFFER_SIZE, freq,prevIn,prevOut
     voice = wav2Float(in_data) #Se pasan a float los samples de la voz
     aM = np.asarray(lazy_lpc.lpc.autocor(voice, 12).numerator)# Se obtienen los coeficientes LPC de la voz
 
     duty = 0.5 # Duty de la onda cuadrada
     Av = 0.1# Amplitud
-    zeros = np.zeros((BUFFER_SIZE,))
+    glotalPulse = np.zeros((frame_count,))
     T = int(wf.getframerate() / freq)# Período
     # Se construye un tren de pulsos de frecuencia determinada
-    for j in range(zeros.shape[0]):
+    for j in range(glotalPulse.shape[0]):
         # ex es la cantidad de muestras por encima de donde deberia comenzar la delta
         ex = ((j + BUFFER_SIZE * buffer_count) % T)
         if (ex / T) < duty:
-            zeros[j] = Av
+            glotalPulse[j] = Av
+    #glotalPulse = wav2Float(wf.readframes(frame_count)) #Si se quiere usar música como input
 
+    input=np.multiply(np.concatenate((prevIn,glotalPulse)),np.hamming(int(BUFFER_SIZE)))
 
-    outPrev, zn = signal.lfilter(np.array([1]), aM, zeros, zi=zi) #Filtrado previo para aplicar control de ganancia
-    gain=np.std(outPrev)/np.std(zeros);
-    out, zi = signal.lfilter(np.array([1/gain]), aM, zeros, zi=zi) #Filtrado final
+    out4Gain, zn = signal.lfilter(np.array([1]), aM, input, zi=zi) #Filtrado previo para aplicar control de ganancia
+    gain=np.std(out4Gain)/np.std(input);
+    out, zi = signal.lfilter(np.array([1/gain]), aM, input, zi=zi) #Filtrado final
 
+    FinaulOut = prevOut + out[0:frame_count]
+    prevIn = glotalPulse
+    prevOut = out[frame_count:]
     buffer_count += 1
 
-    return float2wav(out), pyaudio.paContinue
+    return float2wav(FinaulOut), pyaudio.paContinue
 
 
 wf = wave.open("GH3-SGB.wav", 'rb') # Archivo de música que va a leer
@@ -61,7 +67,7 @@ p = pyaudio.PyAudio()
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 channels=wf.getnchannels(),
                 rate=wf.getframerate(),
-                frames_per_buffer=BUFFER_SIZE,
+                frames_per_buffer=int(BUFFER_SIZE/2),
                 output=True,
                 input=True,
                 stream_callback=callback)
